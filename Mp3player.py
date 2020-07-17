3d1988c9f8e1537ae1e0543ead46a0ee02aca8f3
Ravi Kumar <57840416+ravi-poddar@users.noreply.github.com>
2020-07-17 16:22:32 +0530

    play, pause , resume, stop all functionality added

--- Mp3player.py
+++ Mp3player.py
@@ -1,6 +1,7 @@
 # Importing Modules
 import pygame
 import tkinter as tkr
+import os
 
 # Creating Window
 MusicPlayer = tkr.Tk()
@@ -9,27 +10,56 @@ MusicPlayer = tkr.Tk()
 MusicPlayer.title("Mp3 Player")
 MusicPlayer.geometry("300x400")
 
-# Songs Events
-file="Song.mp3"
+# Songs Playlist Directory set
+file = "C:/Users/Ravi/PycharmProjects/mp3-Player"
+os.chdir(file)
+# print(os.getcwd)
+songsList = os.listdir()
 
+# Songs Playlist
+playList = tkr.Listbox(MusicPlayer, highlightcolor="blue", selectmode = tkr.SINGLE)
+print(songsList)
+for song in songsList:
+    pos = 0
+    playList.insert(pos, song)
+    pos = pos + 1
+
+# pygame init
+pygame.init()
+pygame.mixer.init()
+
+# Song Action Events
 def playSong():
-    pygame.init()
-    pygame.mixer.init()
-    pygame.mixer.music.load(file)
+    pygame.mixer.music.load(playList.get(tkr.ACTIVE))
+    songName.set(playList.get(tkr.ACTIVE))
     pygame.mixer.music.play()
 
 def stopSong():
     pygame.mixer.music.stop()
 
+def pauseSong():
+    pygame.mixer.music.pause()
+
+def resumeSong():
+    pygame.mixer.music.unpause()
+
 # Mp3 Player Layout
 Button1 = tkr.Button(MusicPlayer, width=5, height=3, text="PLAY", command=playSong)
-Button1.pack(fill="x")
 Button2 = tkr.Button(MusicPlayer, width=5, height=3, text="STOP", command=stopSong)
-Button2.pack(fill="x")
-label1 = tkr.LabelFrame(MusicPlayer, text="Song Name")
-label1.pack(fill="both", expand="yes")
-content1 = tkr.Label(label1, text=file)
-content1.pack()
+Button3 = tkr.Button(MusicPlayer, width=5, height=3, text="PAUSE", command=pauseSong)
+Button4 = tkr.Button(MusicPlayer, width=5, height=3, text="RESUME", command=resumeSong)
 
+# song name
+songName = tkr.StringVar()
+songTitle = tkr.Label(MusicPlayer, textvariable = songName)
+
+
+# Place Widgets
+songTitle.pack()
+Button1.pack(fill="x")
+Button2.pack(fill="x")
+Button3.pack(fill="x")
+Button4.pack(fill="x")
+playList.pack(fill="both", expand="yes")
 # Activate Mp3 Player
 MusicPlayer.mainloop()
